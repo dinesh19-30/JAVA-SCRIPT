aa7df7c561f11dfcaf2382590a8ded9aad2c0155
dinesh19-30 <dineshhariraman2004@gmail.com>
2025-08-26 21:15:07 +0530

    datatype

---
+++ datatype.js
@@ -0,0 +1,16 @@
+//int
+var a=19
+//string
+var b="dinesh"
+//boolean
+var c=true
+//object
+var d=null
+//underfined
+var e
+console.log(a,b,c,d,e)
+console.log(typeof(a))
+console.log(typeof(b))
+console.log(typeof(c))
+console.log(typeof(d))
+console.log(typeof(e))
