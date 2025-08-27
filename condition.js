cb3d3bd51fa71d2f0c2b896b3db7d3627dcc2a38
dinesh19-30 <dineshhariraman2004@gmail.com>
2025-08-27 21:29:16 +0530

    condition

--- condition.js
+++ condition.js
@@ -6,3 +6,34 @@ if(false){
 else{
     console.log("its false")
 }
+// condition examples
+
+var rain=true
+if(rain){
+    console.log("Take an umbrella")
+}
+else{
+    console.log("enjoy the sunshine")
+}
+
+var homework=true
+if(homework){
+    console.log("Great job!")
+}
+else{
+    console.log("finish your homework before playing")
+}
+
+//ex2
+
+var color="red"
+if(color=="red"){
+    console.log("stop")
+}
+
+if(color=="yellow"){
+    console.log("get ready")
+}
+if(color=="green"){
+    console.log("start")
+}
